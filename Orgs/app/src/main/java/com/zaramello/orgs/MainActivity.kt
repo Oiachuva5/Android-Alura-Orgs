95bd91dc335918212ea2edd03b4cf26d74a2e4c7
oiachuva5 <oiachuva5@gmail.com>
2024-02-04 18:18:15 -0300

    first commit

---
+++ Orgs/app/src/main/java/com/zaramello/orgs/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.zaramello.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Bem vindo(a) ao Orgs", Toast.LENGTH_SHORT).show()
+        val view = TextView(this)
+        view.setText("olá")
+        setContentView(view)
+    }
+}
