9649e7bc76ea88bd2ee37e6b519fc6b2d2fcbcef
oiachuva5 <oiachuva5@gmail.com>
2024-02-17 20:07:04 -0300

    Adicinado campos de texto no formulario e botão de salvar

--- Orgs/app/src/main/java/com/zaramello/orgs/ui/activity/MainActivity.kt
+++ Orgs/app/src/main/java/com/zaramello/orgs/ui/activity/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.zaramello.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.zaramello.orgs.R
 import com.zaramello.orgs.model.ProductModel
 import com.zaramello.orgs.ui.recyclerView.adapter.ProductListAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ProductListAdapter(context = this, products = listOf(
             ProductModel(nome = "teste",
